a7948d8d6b5877ca1af25517a19e0838235bcce5
zukie nkalitshana <zukie.tn@gmail.com>
2019-08-09 14:54:04 +0200

    Add project files.

---
+++ EmployeeTrainingProgram/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeTrainingProgram.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EmployeeTrainingProgram/Models/TrainingProgram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeTrainingProgram.Models
+{
+    public class TrainingProgram
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProgramID { get; set; }
+
+        [Required(ErrorMessage = "Certificate name is required.")]
+        [StringLength(70)]
+        [DisplayName("Certificate Name")]
+        public string CertificateName { get; set; }
+
+        [StringLength(20)]
+        public string Group { get; set; }
+
+        [DisplayName("Issue Date")]
+        [DataType(DataType.Date)]
+        public DateTime IssueDate { get; set; }
+
+        [DisplayName("Expiry Date")]
+        [DataType(DataType.Date)]
+        public DateTime ExpiryDate { get; set; }
+
+        public string Status { get; set; }
+    }
+}
